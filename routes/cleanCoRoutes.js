11b0da7d058f0b737c3c484202eee2fb8276418d
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-07-09 18:02:08 +0600

    jwt added and token varification

--- routes/cleanCoRoutes.js
+++ routes/cleanCoRoutes.js
@@ -1,9 +1,28 @@
 const express = require("express");
-const router = express.Router();
+const jwt = require("jsonwebtoken");
 const { getCollection } = require("../config/db");
 const { ObjectId } = require("mongodb");
 
-//     <--GET ROUTES ENDPOINT-->
+const router = express.Router();
+
+// Middleware for JWT verification
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// <--GET ROUTES ENDPOINT-->
 
 // Welcome Route to test connection
 router.get("/", (req, res) => {
@@ -17,19 +36,24 @@ router.get("/services", async (req, res) => {
   res.send(result);
 });
 
-// Route to fetch services
-router.get("/user/all-booking", async (req, res) => {
-    const bookingCollection = getCollection("bookings");
-    const result = await bookingCollection.find({}).toArray();
-    res.send(result);
+// Route to fetch all bookings
+router.get("/user/all-booking", verifyToken, async (req, res) => {
+  const bookingCollection = getCollection("bookings");
+  const result = await bookingCollection.find({}).toArray();
+  res.send(result);
 });
 
-//     <--POST ROUTES ENDPOINT-->
+// <--POST ROUTES ENDPOINT-->
 
 // JWT create and send to the client
-router.post('/auth/access-token', async (req, res) => {
-    
-})
+router.post("/auth/access-token", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
+    res.send({ token });
+    console.log(token);
+});
 
 // Route to create a new booking from user
 router.post("/user/create-booking", async (req, res) => {
@@ -39,7 +63,7 @@ router.post("/user/create-booking", async (req, res) => {
   res.send(result);
 });
 
-//      <--DELETE ROUTES ENDPOINT-->
+// <--DELETE ROUTES ENDPOINT-->
 
 // Route to delete a booking from user
 router.delete("/user/cancel-booking/:bookingId", async (req, res) => {
